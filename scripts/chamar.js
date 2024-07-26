067f34186a036c4652f073067e4a45039fca7c5e
lopes7890 <lopesallison414@gmail.com>
2024-07-26 18:26:38 -0300

    Revert "Resolvendo erro no Github"
    This reverts commit 21ab1aeef31f566d14bba1823df209cf0e9821e1.

--- scripts/chamar.js
+++ scripts/chamar.js
@@ -1,7 +1,7 @@
 async function chamar(){
   let valor = document.getElementById("valor").value
   localStorage.setItem('valor', valor)
-  window.location.href = 'tcc-etec-ds/resultado.html'
+  window.location.href = 'resultado.html'
 }
 
 function w3_open() {
